c04958082bf0665d307cc20f83d2ca9835e70929
levien17695 <66364767+levien17695@users.noreply.github.com>
2020-06-05 13:56:33 +0700

    bai tap ve nha buoi 2

---
+++ Untitled3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+	int a,b,c;
+	scanf ("%d", &a);
+	scanf ("%d", &b);
+	scanf ("%d", &c);
+	a*x*x+b*x+c=0;
+	if (a=0){
+		if (b!=0){
+			x= - b/c
+			printf("ket qua x bang: %f, x");
+		}else{
+			x
+			print
+			
+		}
+		
+		
---
+++ tim so lon nhat trong 3 so.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int a,b,c ;
+	scanf ("%d", & a);
+	scanf ("%d", & b);
+	scanf ("%d", & c);
+	if (a>b){
+		if (a>c){
+			printf ("a la so lon nhat");
+			}else{
+				printf ("c la so lon nhat");
+			}
+	    }else{
+	    	if(b>c){
+	    		printf ("b la so lon nhat");
+				}else{
+				printf ("c la so lon nhat");
+				}
+			}
+	    	
+}
+
+	
+
